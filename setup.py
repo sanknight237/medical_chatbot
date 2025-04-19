ce4dade90e291dc151c461de21b2d38e74a3d322
sanknight237 <surajsingh02541@gmail.com>
2025-04-19 15:04:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Suraj Singh Chahar",
+    author_email="surajsingh02541@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
